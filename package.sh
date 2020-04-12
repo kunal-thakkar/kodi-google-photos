5f188aedd5e5f71712f7222111cb835f6893be34
Kunal <kunalmthakkar@gmail.com>
2020-04-12 15:53:19 +0530

    .gitignore fix

--- package.sh
+++
@@ -1,4 +0,0 @@
-mkdir plugin.picture.picasa4kodi
-cp -r `ls | grep -v ".git" | grep -v "plugin.picture.picasa4kodi"` plugin.picture.picasa4kodi/
-zip -r ../plugin.picture.picasa4kodi.zip plugin.picture.picasa4kodi/
-rm -rf plugin.picture.picasa4kodi/
